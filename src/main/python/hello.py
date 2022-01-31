6711599c8b39539b23eb4118967496cb25823d2c
Veera Shekar Gutthula <gveerashekar@hotmail.com>
2022-01-31 20:23:07 +0530

    old changes from stash

--- src/main/python/hello.py
+++ src/main/python/hello.py
@@ -1,5 +1,9 @@
 print("welcome to new world")
 print("hai how are you veera shekar")
+<<<<<<< Updated upstream
 print ("Great")
 
 print ("Ram")
+=======
+print ("Great done well")
+>>>>>>> Stashed changes
